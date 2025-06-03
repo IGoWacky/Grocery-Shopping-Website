8d99d5a0a66e32dce1e1b5e87bcd9630d6dd46aa
sekun <sekunoshikanlu@gmail.com>
2025-06-03 14:42:55 -0400

    Created and began products page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -14,6 +14,7 @@ export const AppContextProvider = ({children}) => {
     const [showUserLogin, setShowUserLogin] = useState(false)
     const [products, setProducts] = useState([])
     const [cartItems, setCartItems] = useState({})
+    const [searchQuery, setSearchQuery] = useState({})
 
     const fetchProducts = async () => {
         setProducts(dummyProducts)
@@ -53,7 +54,8 @@ export const AppContextProvider = ({children}) => {
         fetchProducts()
     }, [])
 
-    const value = {navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency, cartItems, addToCart, updateCartItem, removeFromCart}
+    const value = {navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products,
+        currency, cartItems, addToCart, updateCartItem, removeFromCart, searchQuery, setSearchQuery}
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
