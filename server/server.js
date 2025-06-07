d4d4740b6ec63750131a0bcf4c40a21b9dde6c79
sekun <sekunoshikanlu@gmail.com>
2025-06-07 10:44:48 -0400

    connected mongodb

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import cors from 'cors';
+import connectDB from './configs/db.js';
+import 'dotenv/config';
+import userRouter from './routes/UserRoute.js';
+
+const app = express();
+const port = process.env.PORT || 4000;
+await connectDB();
+const allowedOrigins = ["http://localhost:5173"]
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({origin: allowedOrigins, credentials: true}))
+app.get('/', (req, res) => res.send("API is working"));
+app.use("/api/user", userRouter)
+app.listen(port, () => {
+    console.log(`Running server on http://localhost:${port}`)
+})
