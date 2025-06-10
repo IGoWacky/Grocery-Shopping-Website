05828ac0d4067d332a38e56796a25a20a4699205
sekun <sekunoshikanlu@gmail.com>
2025-06-10 02:57:47 -0400

    cart and product controllers/api

--- server/server.js
+++ server/server.js
@@ -5,10 +5,14 @@ import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/UserRoute.js';
 import sellerRouter from './routes/SellerRoute.js';
+import connectCloudinary from './configs/cloudinary.js';
+import productRouter from './routes/ProductRoute.js';
+import cartRouter from './routes/CartRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
 await connectDB();
+await connectCloudinary();
 const allowedOrigins = ["http://localhost:5173"]
 
 app.use(express.json())
@@ -17,6 +21,9 @@ app.use(cors({origin: allowedOrigins, credentials: true}))
 app.get('/', (req, res) => res.send("API is working"));
 app.use("/api/user", userRouter)
 app.use("/api/seller", sellerRouter)
+app.use("/api/product", productRouter)
+app.use("/api/cart", cartRouter)
+
 app.listen(port, () => {
     console.log(`Running server on http://localhost:${port}`)
 })
